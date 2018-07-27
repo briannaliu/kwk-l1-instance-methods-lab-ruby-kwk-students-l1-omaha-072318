60e47a5bb964d192682be2a9c7797596050d70fe
briannaliu <briannaliu3@gmail.com>
2018-07-27 18:49:44 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # dog.rb
+class Dog
+  def bark
+    puts "Woof!"
+  end
+  
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+
+
+
+
+
+  
+  
+  
+  # class Snacks
+#   def initialize(name, taste = "yummy")
+#     @name = name
+#     @type = taste
+#     puts "These #{@name} are so #{@type}."
+#   end
+# end
+
+# chips = Snacks.new("takis", "spicy")
