bc9bf6c3160e9729791c2d2182484e8255ec45ee
briannaliu <briannaliu3@gmail.com>
2018-07-27 18:34:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 # dog.rb
 class Dog
+  def initialize(name, )
   
+  
+  
+  
+  # class Snacks
+#   def initialize(name, taste = "yummy")
+#     @name = name
+#     @type = taste
+#     puts "These #{@name} are so #{@type}."
+#   end
+# end
+
+# chips = Snacks.new("takis", "spicy")
