1158cb3c5eef8683d9d9712a7e9584abc0009cf2
briannaliu <briannaliu3@gmail.com>
2018-07-27 18:43:39 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,9 @@
 # person.rb
-class Dog
+class Person
   def talk
     puts "Hello World!"
   end
-  def initialize(name, action = "sit")
+  def initialize(name, action = "walk")
     @name = name
     @type = action
     puts "The #{name} is #{action}ing"
